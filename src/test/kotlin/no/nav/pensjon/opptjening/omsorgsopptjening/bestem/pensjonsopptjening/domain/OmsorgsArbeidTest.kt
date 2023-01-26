fe0d3d0868b367adc3cd587eb2494712062baf07
eivind <eivind.askestad@nav.no>
2023-01-26 16:43:16 +0100

    Parameterized tests for calculation of months
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- src/test/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/domain/OmsorgsArbeidTest.kt
+++ src/test/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/domain/OmsorgsArbeidTest.kt
@@ -7,17 +7,46 @@ import no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.Utb
 import no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.domain.factory.OmsorgsArbeidFactory
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import java.time.LocalDate
 import java.time.Month
 
 internal class OmsorgsArbeidTest {
 
 
-    @Test
-    fun `Given omsorgsarbeid for 6 monts When calling monthsOfOmsorg Then six`() {
-        val omsorgsArbeid = OmsorgsArbeidFactory.createOmsorgsArbeid(creatOmsorgsArbeidModel())
+    @ParameterizedTest
+    @CsvSource(
+            "2020-01-01, 2020-01-01, 1",
+            "2020-01-01, 2020-02-01, 2",
+            "2020-01-01, 2020-03-01, 3",
+            "2020-01-01, 2020-04-01, 4",
+            "2020-01-01, 2020-05-01, 5",
+            "2020-01-01, 2020-06-01, 6",
+            "2020-01-01, 2020-07-01, 7",
+            "2020-01-01, 2020-08-01, 8",
+            "2020-01-01, 2020-09-01, 9",
+            "2020-01-01, 2020-10-01, 10",
+            "2020-01-01, 2020-11-01, 11",
+            "2020-01-01, 2020-12-01, 12",
+    )
+    fun `Given omsorgsarbeid When calling monthsOfOmsorg Then return months of omsorgsarbeid`(
+        fom: String,
+        tom: String,
+        expectedAmountOfMoths: Long
+    ) {
+        val omsorgsArbeid = OmsorgsArbeidFactory.createOmsorgsArbeid(
+            creatOmsorgsArbeidModel(
+                utbetalingsPeriode = listOf(
+                    creatUtbetalingsPeriodeModel(
+                        fom = LocalDate.parse(fom),
+                        tom = LocalDate.parse(tom)
+                    )
+                )
+            )
+        )
 
-        assertEquals(6, omsorgsArbeid.monthsOfOmsorg())
+        assertEquals(expectedAmountOfMoths, omsorgsArbeid.monthsOfOmsorg())
     }
 
     @Test
@@ -29,27 +58,10 @@ internal class OmsorgsArbeidTest {
         assertEquals(0, omsorgsArbeid.monthsOfOmsorg())
     }
 
-    @Test
-    fun `Given omsorgsarbeid for 12 monts When calling monthsOfOmsorg Then 12`() {
-        val omsorgsArbeid = OmsorgsArbeidFactory.createOmsorgsArbeid(
-            creatOmsorgsArbeidModel(utbetalingsPeriode = listOf(
-                creatUtbetalingsPeriodeModel(
-                    fom = LocalDate.of(2020, Month.JANUARY, 1),
-                    tom = LocalDate.of(2020, Month.DECEMBER, 1)
-                )
-            ))
-        )
-
-        assertEquals(12, omsorgsArbeid.monthsOfOmsorg())
-    }
-
-
     // TODO Parameterized tests og flere edge caser.
     // Hva betyr fom og tom for BA
 
     private fun creatOmsorgsArbeidModel(
-        fom: LocalDate = LocalDate.of(2020, Month.JANUARY, 1),
-        tom: LocalDate = LocalDate.of(2020, Month.JUNE, 1),
         utbetalingsPeriode: List<UtbetalingsPeriodeModel> = listOf(creatUtbetalingsPeriodeModel())
 
     ) = OmsorgsArbeidModel(
