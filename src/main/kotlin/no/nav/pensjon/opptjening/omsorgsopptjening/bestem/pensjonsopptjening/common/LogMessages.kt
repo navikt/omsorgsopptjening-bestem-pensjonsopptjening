904b3fe951901324e087a7ad5cecf5dcc8415fc5
eivind <eivind.askestad@nav.no>
2023-01-31 18:45:54 +0100

    Removed common log message

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/common/LogMessages.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.common
-
-class LogMessages {
-    companion object {
-        val seSecureLoggMelding: String get() = """Se secure log for mer informasjon"""
-    }
-
-}
