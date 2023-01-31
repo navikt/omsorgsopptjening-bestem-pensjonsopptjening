f17c766b9d433c22135147314642da93dfcb6728
eivind <eivind.askestad@nav.no>
2023-01-31 12:18:28 +0100

    Equals method on fnr

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/domain/person/Fnr.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/domain/person/Fnr.kt
@@ -1,6 +1,6 @@
 package no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.domain.person
 
-class Fnr(private val fnr:String){
+class Fnr(private val fnr: String) {
 
-
+    override fun equals(other: Any?) = other === this || (other is Fnr && other.fnr == fnr)
 }
--- src/test/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/domain/person/FnrTest.kt
+++ src/test/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/domain/person/FnrTest.kt
@@ -1,7 +1,18 @@
 package no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.domain.person
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 
-internal class FnrTest{
+internal class FnrTest {
 
+    @Test
+    fun `Given two equal fnr numbers When calling equals than return true`() {
+        assertEquals( Fnr("12345678901"), Fnr("12345678901"))
+    }
+
+    @Test
+    fun `Given same object When calling equals than return true`() {
+        val fnr = Fnr("12345678901")
+        assertEquals(fnr, fnr)
+    }
 }
