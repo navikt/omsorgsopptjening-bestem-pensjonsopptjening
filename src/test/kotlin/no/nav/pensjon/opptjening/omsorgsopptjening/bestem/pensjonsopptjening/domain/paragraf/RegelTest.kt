4bcadf58121223d0663e44d182470fb91746b67f
eivind <eivind.askestad@nav.no>
2023-02-03 09:11:53 +0100

    Regler med regeloperatorer og test

--- src/test/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/domain/paragraf/RegelTest.kt
+++ src/test/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/domain/paragraf/RegelTest.kt
@@ -1,6 +1,9 @@
 package no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.domain.paragraf
 
+import no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.domain.paragraf.Eller.Companion.eller
 import no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.domain.paragraf.Og.Companion.og
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 internal class RegelTest {
@@ -8,6 +11,7 @@ internal class RegelTest {
     @Test
     fun bareforATeste() {
         val antallMonederMedBarnetygd = 6
+        val erMedlem = false;
 
         val seksMonederRegel = Regel(
             regelInformasjon = RegelInformasjon(
@@ -19,10 +23,39 @@ internal class RegelTest {
             oppfyllerRegler = MINST_ET_HALVT_AR
         )
 
-        val resultat = og(
-            seksMonederRegel,
-            seksMonederRegel
+        val erFolketrygdetRegel = Regel(
+            regelInformasjon = RegelInformasjon(
+                beskrivelse = "Bruker må være medlem",
+                begrunnelseForInnvilgelse = "Bruker er medlem",
+                begrunnesleForAvslag = "Bruker er ikke medlem",
+            ),
+            inputVerdi = erMedlem,
+            oppfyllerRegler = { it }
         )
+
+        val ResultatOg = og(
+            seksMonederRegel,
+            erFolketrygdetRegel
+        )
+
+        val bareForÅPRøveResultat = og(
+            eller(
+                seksMonederRegel,
+                seksMonederRegel,
+            ),
+            eller(
+                seksMonederRegel,
+                erFolketrygdetRegel,
+            )
+        )
+
+        val resultatEller = eller(
+            seksMonederRegel,
+            erFolketrygdetRegel,
+        )
+
+        assertFalse(ResultatOg.brukRegel().oppFyllerRegel)
+        assertTrue(resultatEller.brukRegel().oppFyllerRegel)
 
         println("")
 
