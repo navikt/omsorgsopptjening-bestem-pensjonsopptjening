95da300a2d4270c45b6a51a9d5927b8b41b3bb71
eivind <eivind.askestad@nav.no>
2023-02-20 15:52:07 +0100

    Using spring to load graphql resource
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/person/pdl/PdlQuery.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/person/pdl/PdlQuery.kt
@@ -1,12 +1,15 @@
 package no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.person.pdl
 
-fun createQuery(fnr: String): PdlQuery {
-    val query = PdlQuery::class.java.getResource("/pdl/queries/folkeregisteridentifikator.graphql")
-        .readText()
-        .replace("[\n\r]", "")
-    return PdlQuery(query, FnrVariables(fnr))
-}
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.io.Resource
 
+
+class Query(@Value("classpath:data/resource-data.txt") private val resourceFile: Resource){
+    fun createQuery(fnr: String): PdlQuery {
+        val query = String(resourceFile.file.readBytes()).replace("[\n\r]", "")
+        return PdlQuery(query, FnrVariables(fnr))
+    }
+}
 data class PdlQuery(val query: String, val variables: FnrVariables)
 
 data class FnrVariables(val ident: String)
