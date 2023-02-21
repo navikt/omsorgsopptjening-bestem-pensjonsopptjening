220827afec8789c08f086177462fc0b9579fd670
Mo Amini <mo.amini@nav.no>
2023-02-21 09:10:13 +0100

    Gjør pdlclient til spring komponent
    Co-authored-by: eivind.askestad@nav.no

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/person/pdl/PdlClient.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/person/pdl/PdlClient.kt
@@ -1,4 +1,9 @@
 package no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.person.pdl
 
-class PdlClient {
+import org.springframework.stereotype.Component
+import org.springframework.web.client.RestTemplate
+
+@Component
+class PdlClient(restTemplate: RestTemplate) {
+
 }
