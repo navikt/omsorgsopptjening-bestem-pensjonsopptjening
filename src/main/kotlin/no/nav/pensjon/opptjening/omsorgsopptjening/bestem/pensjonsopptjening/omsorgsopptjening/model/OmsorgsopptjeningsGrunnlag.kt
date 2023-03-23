d775f1017dae58ca81ea570a7445e2473f3c0283
eivind <eivind.askestad@nav.no>
2023-03-23 09:44:33 +0100

    OmsorgsopptjeningsGrunnlag JPA entity
    Co-authored-by: Mo Amini <mo.amini@nav.no>

---
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/omsorgsopptjening/model/OmsorgsopptjeningsGrunnlag.kt
@@ -0,0 +1,29 @@
+package no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.omsorgsopptjening.model
+
+import jakarta.persistence.*
+import no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.person.model.Person
+
+@Entity
+@Table(name = "OMSORGSOPPTJENINGSGRUNNLAG")
+data class OmsorgsopptjeningsGrunnlag(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "OMSORGSOPPTJENINGSGRUNNLAG_ID", nullable = false)
+    var id: Long? = null,
+
+    @Column(name = "OMSORGS_AR", nullable = false)
+    val omsorgsAr: Int,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "STATUS", nullable = false)
+    val status: Status,
+
+    @ManyToMany
+    @JoinTable(
+        name = "INVOLVERTE_PERSONER",
+        joinColumns = [JoinColumn(name = "OMSORGSOPPTJENINGSGRUNNLAG_ID", referencedColumnName = "OMSORGSOPPTJENINGSGRUNNLAG_ID")],
+        inverseJoinColumns = [JoinColumn(name = "PERSON_ID", referencedColumnName = "PERSON_ID")]
+    )
+    val insvolvertePersoner: List<Person>
+)
---
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/omsorgsopptjening/model/Status.kt
@@ -0,0 +1,6 @@
+package no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.omsorgsopptjening.model
+
+enum class Status {
+    FERDIG_BEHANDLET,
+    TRENGER_INFORMASJON
+}
