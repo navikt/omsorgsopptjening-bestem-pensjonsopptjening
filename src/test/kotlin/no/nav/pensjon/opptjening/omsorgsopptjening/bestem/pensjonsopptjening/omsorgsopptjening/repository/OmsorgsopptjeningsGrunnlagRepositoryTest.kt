a65101b8cc1e083a218a0acbeaf210f70d4716e9
eivind <eivind.askestad@nav.no>
2023-03-28 15:38:07 +0200

    Tests for overriding omsorgsopptjeningsGrunnlag for two persons

--- src/test/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/omsorgsopptjening/repository/OmsorgsopptjeningsGrunnlagRepositoryTest.kt
+++ src/test/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/omsorgsopptjening/repository/OmsorgsopptjeningsGrunnlagRepositoryTest.kt
@@ -41,7 +41,7 @@ internal class OmsorgsopptjeningsGrunnlagRepositoryTest {
     }
 
     @Test
-    fun `Given person with omsorgsopptjeningsGrunnlag when findByInvolvertePersoner then return omsorgsopptjeningsGrunnlag`() {
+    fun `Given saved omsorgsopptjeningsGrunnlag W hen findBy then return omsorgsopptjeningsGrunnlag`() {
         val person = personRepository.updatePerson(person1)
 
         repository.save(
@@ -60,7 +60,7 @@ internal class OmsorgsopptjeningsGrunnlagRepositoryTest {
     }
 
     @Test
-    fun `Given two persons in same OmsorgsopptjeningsGrunnlag then return same grunnlag for both persons `() {
+    fun `Given saved OmsorgsopptjeningsGrunnlag related to two persons When findBy Then return same grunnlag for both persons`() {
         val person1 = personRepository.updatePerson(person1)
         val person2 = personRepository.updatePerson(person2)
 
@@ -72,7 +72,6 @@ internal class OmsorgsopptjeningsGrunnlagRepositoryTest {
             )
         )
 
-
         val grunnlagPerson1 = repository.findBy(personer = listOf(person1), omsorgsAr = `2020`)
         val grunnlagPerson2 = repository.findBy(personer = listOf(person2), omsorgsAr = `2020`)
 
@@ -83,7 +82,7 @@ internal class OmsorgsopptjeningsGrunnlagRepositoryTest {
     }
 
     @Test
-    fun `given ovverriding grunnlag for ar then set old to historisk`() {
+    fun `When overriding grunnlag for ar Then set old to historisk`() {
         val person = personRepository.updatePerson(person1)
 
         repository.save(
@@ -109,15 +108,52 @@ internal class OmsorgsopptjeningsGrunnlagRepositoryTest {
         assertEquals(person.gjeldendeFnr, grunnlag.first().involvertePersoner.first().gjeldendeFnr)
         assertFalse(grunnlag.first().historisk)
 
-        val historiskGrunnlag = repository.findBy(personer = listOf(person), omsorgsAr = `2020`, historisk = true)
-        assertEquals(1, historiskGrunnlag.size)
-        assertEquals(1, historiskGrunnlag.first().involvertePersoner.size)
-        assertEquals(person.gjeldendeFnr, historiskGrunnlag.first().involvertePersoner.first().gjeldendeFnr)
-        assertTrue(historiskGrunnlag.first().historisk)
+        val oldGrunnlag = repository.findBy(personer = listOf(person), omsorgsAr = `2020`, historisk = true)
+        assertEquals(1, oldGrunnlag.size)
+        assertEquals(1, oldGrunnlag.first().involvertePersoner.size)
+        assertEquals(person.gjeldendeFnr, oldGrunnlag.first().involvertePersoner.first().gjeldendeFnr)
+        assertTrue(oldGrunnlag.first().historisk)
     }
 
     @Test
-    fun `OmsorgsopptjeningsGrunnlagRepository should save unsaved persons`() {
+    fun `When overriding grunnlag for two persons Then set old to historisk`() {
+        val person1 = personRepository.updatePerson(person1)
+        val person2 = personRepository.updatePerson(person2)
+
+        repository.save(
+            OmsorgsopptjeningsGrunnlag(
+                omsorgsAr = `2020`,
+                status = Status.TRENGER_INFORMASJON,
+                involvertePersoner = listOf(person1, person2)
+            )
+        )
+
+        repository.save(
+            OmsorgsopptjeningsGrunnlag(
+                omsorgsAr = `2020`,
+                status = Status.FERDIG_BEHANDLET,
+                involvertePersoner = listOf(person1, person2)
+            )
+        )
+
+
+        val grunnlag = repository.findBy(personer = listOf(person1, person2), omsorgsAr = `2020`, historisk = false)
+        assertEquals(1, grunnlag.size)
+        assertEquals(2, grunnlag.first().involvertePersoner.size)
+        assertTrue(grunnlag.first().involvertePersoner.any{it.erSammePerson(person1)})
+        assertTrue(grunnlag.first().involvertePersoner.any{it.erSammePerson(person2)})
+        assertFalse(grunnlag.first().historisk)
+
+        val oldGrunnlag = repository.findBy(personer = listOf(person1, person2), omsorgsAr = `2020`, historisk = true)
+        assertEquals(1, oldGrunnlag.size)
+        assertEquals(2, oldGrunnlag.first().involvertePersoner.size)
+        assertTrue(oldGrunnlag.first().involvertePersoner.any{it.erSammePerson(person1)})
+        assertTrue(oldGrunnlag.first().involvertePersoner.any{it.erSammePerson(person2)})
+        assertTrue(oldGrunnlag.first().historisk)
+    }
+
+    @Test
+    fun `OmsorgsopptjeningsGrunnlagRepository should not save unsaved persons`() {
         val e = assertThrows<InvalidDataAccessApiUsageException> {
             repository.save(
                 OmsorgsopptjeningsGrunnlag(
