af215ec54ffce89422718cdd6c5a55e16d88b5c5
eivind <eivind.askestad@nav.no>
2023-04-03 14:38:42 +0200

    Remove unused class

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/omsorgsarbeid/model/OmsorgsarbeidsInformasjon.kt
+++
@@ -1,6 +0,0 @@
-package no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.omsorgsarbeid.model
-
-data class OmsorgsarbeidsInformasjon(
-    val omsorgsarbeidSnapshot: OmsorgsarbeidSnapshot,
-    val relaterteOmsorgsarbeidSnapshot: List<OmsorgsarbeidSnapshot> = listOf()
-)
