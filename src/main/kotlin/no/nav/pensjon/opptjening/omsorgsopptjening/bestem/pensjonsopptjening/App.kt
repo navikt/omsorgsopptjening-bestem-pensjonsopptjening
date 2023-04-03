c845f463588fd0f0ba72ce3c91be37d3cf447fa5
eivind <eivind.askestad@nav.no>
2023-04-03 09:28:02 +0200

    Moved DB startup config to App
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/App.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/App.kt
@@ -1,13 +1,39 @@
 package no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening
 
+import jakarta.persistence.EntityManagerFactory
+import org.flywaydb.core.Flyway
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.jdbc.support.DatabaseStartupValidator
 import org.springframework.retry.annotation.EnableRetry
-
-@EnableRetry
-@SpringBootApplication
-class App
+import javax.sql.DataSource
 
 fun main(args: Array<String>) {
     runApplication<App>(*args)
+}
+
+@EnableRetry
+@SpringBootApplication
+class App {
+
+    @Bean
+    fun databaseStartupValidator(dataSource: DataSource) = DatabaseStartupValidator().apply {
+        setDataSource(dataSource)
+    }
+
+    @Bean
+    fun dependsOnPostProcessor(): BeanFactoryPostProcessor? {
+        return BeanFactoryPostProcessor { bf: ConfigurableListableBeanFactory ->
+            val flyway = bf.getBeanNamesForType(Flyway::class.java)
+            flyway.map { beanName: String? -> bf.getBeanDefinition(beanName!!) }
+                .forEach { it.setDependsOn("databaseStartupValidator") }
+
+            val jpa = bf.getBeanNamesForType(EntityManagerFactory::class.java)
+            jpa.map { beanName: String? -> bf.getBeanDefinition(beanName!!) }
+                .forEach { it.setDependsOn("databaseStartupValidator") }
+        }
+    }
 }
