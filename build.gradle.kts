e0b997b6e3965b04977423827becc98c56835421
Mo Amini <mo.amini@nav.no>
2023-06-23 08:39:16 +0200

    Bumper domene lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val domeneVersion = "1.0.39"
+val domeneVersion = "1.0.40"
 val azureAdClient = "0.0.7"
 val jacksonVersion = "2.14.2"
 val logbackEncoderVersion = "7.3"
