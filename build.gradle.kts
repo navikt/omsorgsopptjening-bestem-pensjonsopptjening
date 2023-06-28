768488f9016d4503082ed901c134b4371d54fe4a
Jacob Meidell <jacob.meidell@nav.no>
2023-06-28 19:18:43 +0200

    bump domene lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val domeneVersion = "1.0.41"
+val domeneVersion = "1.0.48"
 val azureAdClient = "0.0.7"
 val jacksonVersion = "2.14.2"
 val logbackEncoderVersion = "7.3"
