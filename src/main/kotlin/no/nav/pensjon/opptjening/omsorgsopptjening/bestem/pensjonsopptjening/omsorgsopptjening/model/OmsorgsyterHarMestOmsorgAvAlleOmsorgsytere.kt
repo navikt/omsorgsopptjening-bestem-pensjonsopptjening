556e91334bfb1604d5d17c6fc232ec85bb927b0e
Mo Amini <mo.amini@nav.no>
2023-07-03 09:49:44 +0200

    Liten import fix For PersonMedFødselsår

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/omsorgsopptjening/model/OmsorgsyterHarMestOmsorgAvAlleOmsorgsytere.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/omsorgsopptjening/model/OmsorgsyterHarMestOmsorgAvAlleOmsorgsytere.kt
@@ -1,7 +1,5 @@
 package no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.omsorgsopptjening.model
 
-import no.nav.pensjon.opptjening.omsorgsopptjening.felles.domene.PersonMedFødselsår
-
 object OmsorgsyterHarMestOmsorgAvAlleOmsorgsytere :
     ParagrafVilkår<OmsorgsyterHarMestOmsorgAvAlleOmsorgsytere.Grunnlag>() {
     override fun vilkarsVurder(grunnlag: Grunnlag): Vurdering {
