8cfae40268ae94e5bbfa84198a738edfd33038fe
Jacob Meidell <jacob.meidell@nav.no>
2023-09-19 10:36:47 +0200

    Refactor pdlservice/response mapping

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/person/external/pdl/PdlResponse.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/person/external/pdl/PdlResponse.kt
@@ -2,30 +2,97 @@ package no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.pe
 
 import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.omsorgsopptjening.model.Familierelasjon
+import no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.omsorgsopptjening.model.Familierelasjoner
+import no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.omsorgsopptjening.model.Person
 import java.time.LocalDate
 import java.time.LocalDateTime
 
-internal data class PdlResponse(val data: PdlData, private val errors: List<PdlError>? = null) {
+internal data class PdlResponse(
+    val data: PdlData,
+    private val errors: List<PdlError>? = null
+) {
     val error: PdlError? = errors?.firstOrNull()
 }
 
-internal data class PdlData(val hentPerson: HentPersonQueryResponse?)
+internal data class PdlData(
+    val hentPerson: HentPersonQueryResponse?
+)
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 internal data class HentPersonQueryResponse(
     val folkeregisteridentifikator: List<Folkeregisteridentifikator>,
     val foedsel: List<Foedsel>,
     val doedsfall: List<Doedsfall?>,
     val forelderBarnRelasjon: List<ForelderBarnRelasjon>,
-)
+) {
+    private fun gjeldendeIdent(): PdlFnr {
+        return folkeregisteridentifikator
+            .firstOrNull { it.status == Folkeregisteridentifikator.Status.I_BRUK }
+            ?.let { PdlFnr(it.identifikasjonsnummer, gjeldende = true) }
+            ?: throw PdlMottatDataException("Fnr i bruk finnes ikke")
+    }
 
+    private fun bestemDoedsdato(doedsfall: List<Doedsfall?>): LocalDate? {
+        return doedsfall.firstOrNull()?.doedsdato
+    }
+
+    private fun foedselsdato(): LocalDate {
+        return when (foedsel.size) {
+            0 -> {
+                throw PdlMottatDataException("Fødselsår finnes ikke i respons fra pdl")
+            }
+
+            1 -> {
+                LocalDate.parse(foedsel.first().foedselsdato)
+            }
+
+            else -> {
+                LocalDate.parse(foedsel.avklarFoedsel()?.foedselsdato)
+                    ?: throw PdlMottatDataException("Fødselsår finnes ikke i respons fra pdl")
+            }
+        }
+    }
+
+    private fun familierelasjoner(): Familierelasjoner {
+        return forelderBarnRelasjon.map {
+            Familierelasjon(
+                ident = it.relatertPersonsIdent,
+                relasjon = when (it.relatertPersonsRolle) {
+                    ForelderBarnRelasjon.Rolle.BARN -> Familierelasjon.Relasjon.BARN
+                    ForelderBarnRelasjon.Rolle.FAR -> Familierelasjon.Relasjon.FAR
+                    ForelderBarnRelasjon.Rolle.MOR -> Familierelasjon.Relasjon.MOR
+                    ForelderBarnRelasjon.Rolle.MEDMOR -> Familierelasjon.Relasjon.MEDMOR
+                }
+            )
+        }.let {
+            Familierelasjoner(it)
+        }
+    }
+
+    fun toDomain(): Person {
+        return Person(
+            fnr = gjeldendeIdent().fnr,
+            fødselsdato = foedselsdato(),
+            dødsdato = bestemDoedsdato(doedsfall),
+            familierelasjoner = familierelasjoner(),
+        )
+    }
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 internal data class Folkeregisteridentifikator(
     val identifikasjonsnummer: String,
     val status: Status,
     val type: Type,
     val metadata: Metadata,
     val folkeregistermetadata: Folkeregistermetadata? = null,
-)
+) {
+    enum class Status { I_BRUK, OPPHOERT }
+    enum class Type { FNR, DNR }
+}
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 internal data class Foedsel(
     val foedselsaar: Int,
     val foedselsdato: String,
@@ -33,25 +100,41 @@ internal data class Foedsel(
     val folkeregistermetadata: Folkeregistermetadata? = null,
 )
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 internal data class ForelderBarnRelasjon(
     val relatertPersonsIdent: String,
-    val relatertPersonsRolle: String,
-    val minRolleForPerson: String,
+    val relatertPersonsRolle: Rolle,
+    val minRolleForPerson: Rolle,
     val metadata: Metadata,
     val folkeregistermetadata: Folkeregistermetadata? = null,
-)
+) {
+    enum class Rolle {
+        FAR,
+        MOR,
+        MEDMOR,
+        BARN
+    }
+}
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 internal data class Doedsfall(
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     val doedsdato: LocalDate
 )
 
-internal data class Metadata(val historisk: Boolean, val master: String, val endringer: List<Endring> = emptyList())
+@JsonIgnoreProperties(ignoreUnknown = true)
+internal data class Metadata(
+    val historisk: Boolean,
+    val master: String,
+    val endringer: List<Endring> = emptyList()
+)
 
-internal data class Folkeregistermetadata(val ajourholdstidspunkt: LocalDateTime? = null)
+@JsonIgnoreProperties(ignoreUnknown = true)
+internal data class Folkeregistermetadata(
+    val ajourholdstidspunkt: LocalDateTime? = null
+)
 
-internal data class Endring(val registrert: LocalDateTime)
-
-internal enum class Status { I_BRUK, OPPHOERT }
-
-internal enum class Type { FNR, DNR }
+@JsonIgnoreProperties(ignoreUnknown = true)
+internal data class Endring(
+    val registrert: LocalDateTime
+)
--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/person/external/pdl/PdlService.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/person/external/pdl/PdlService.kt
@@ -1,12 +1,9 @@
 package no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.person.external.pdl
 
-import no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.omsorgsopptjening.model.Familierelasjon
-import no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.omsorgsopptjening.model.Familierelasjoner
 import no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.omsorgsopptjening.model.Person
 import no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.person.model.PersonOppslag
 import no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.person.model.PersonOppslagException
 import org.springframework.stereotype.Service
-import java.time.LocalDate
 
 @Service
 internal class PdlService(
@@ -19,40 +16,10 @@ internal class PdlService(
 
             val hentPersonQueryResponse = pdlResponse?.data?.hentPerson ?: throw PdlException(pdlResponse?.error)
 
-            val gjeldende = hentPersonQueryResponse.gjeldendeIdent()
-            val historisk = hentPersonQueryResponse.historisk().filter { it.fnr != gjeldende.fnr }
-
-            return PdlPerson(
-                alleFnr = historisk + gjeldende,
-                fodselsdato = hentPersonQueryResponse.foedselsdato(),
-                doedsdato = bestemDoedsdato(pdlResponse.data.hentPerson.doedsfall),
-                forelderBarnRelasjon = hentPersonQueryResponse.forelderBarnRelasjon()
-            ).let { person ->
-                Person(
-                    fnr = person.gjeldendeFnr,
-                    fødselsdato = person.fodselsdato,
-                    dødsdato = person.doedsdato,
-                    familierelasjoner = person.forelderBarnRelasjon
-                        .map {
-                            Familierelasjon(
-                                ident = it.relatertPersonsIdent,
-                                relasjon = when (it.relatertPersonsRolle) {
-                                    "BARN" -> Familierelasjon.Relasjon.BARN
-                                    "FAR" -> Familierelasjon.Relasjon.FAR
-                                    "MOR" -> Familierelasjon.Relasjon.MOR
-                                    "MEDMOR" -> Familierelasjon.Relasjon.MEDMOR
-                                    else -> throw RuntimeException("Ukjent familierelasjon: ${it.relatertPersonsRolle}")
-                                }
-                            )
-                        }.let {
-                            Familierelasjoner(it)
-                        }
-                )
-            }
+            return hentPersonQueryResponse.toDomain()
         } catch (ex: Throwable) {
             throw PersonOppslagException("Feil ved henting av person", ex)
         }
-
     }
 
     override fun hentAktørId(fnr: String): String {
@@ -67,54 +34,6 @@ internal class PdlService(
             throw PersonOppslagException("Feil ved henting av aktørid", ex)
         }
     }
-
-    private fun bestemDoedsdato(doedsfall: List<Doedsfall?>): LocalDate? {
-        return doedsfall.firstOrNull()?.doedsdato
-    }
-
-    private fun HentPersonQueryResponse.historisk() = folkeregisteridentifikator
-        .filter { it.status == Status.OPPHOERT }
-        .distinctBy { it.identifikasjonsnummer }
-        .map { PdlFnr(it.identifikasjonsnummer, gjeldende = false) }
-
-    private fun HentPersonQueryResponse.gjeldendeIdent() =
-        folkeregisteridentifikator
-            .firstOrNull { it.status == Status.I_BRUK }
-            ?.let { PdlFnr(it.identifikasjonsnummer, gjeldende = true) }
-            ?: throw PdlMottatDataException("Fnr i bruk finnes ikke")
-
-
-    private fun HentPersonQueryResponse.foedselsdato(): LocalDate {
-        return when (foedsel.size) {
-            0 -> {
-                throw PdlMottatDataException("Fødselsår finnes ikke i respons fra pdl")
-            }
-
-            1 -> {
-                LocalDate.parse(foedsel.first().foedselsdato)
-            }
-
-            else -> {
-                LocalDate.parse(foedsel.avklarFoedsel()?.foedselsdato)
-                    ?: throw PdlMottatDataException("Fødselsår finnes ikke i respons fra pdl")
-            }
-        }
-    }
-
-    private fun HentPersonQueryResponse.forelderBarnRelasjon(): List<ForelderBarnRelasjon> {
-        return forelderBarnRelasjon
-    }
-
-}
-
-internal data class PdlPerson(
-    val alleFnr: List<PdlFnr>,
-    val fodselsdato: LocalDate,
-    val doedsdato: LocalDate? = null,
-    val forelderBarnRelasjon: List<ForelderBarnRelasjon>,
-) {
-    val gjeldendeFnr: String get() = alleFnr.first { it.gjeldende }.fnr
-    val historiskeFnr: List<String> get() = alleFnr.filter { !it.gjeldende }.map { it.fnr }
 }
 
 internal data class PdlFnr(val fnr: String, val gjeldende: Boolean)
