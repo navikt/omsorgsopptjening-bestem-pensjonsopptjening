d5e57f5148d7b24e928f8c0e5c9ca48c4e90bd3a
Jan Kroken <jan.kroken@nav.com>
2023-10-20 11:17:43 +0200

    Oppdaterte avhengigheter

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,19 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val domeneVersion = "1.0.60"
 val azureAdClient = "0.0.7"
-val jacksonVersion = "2.15.2"
+val jacksonVersion = "2.15.3"
 val logbackEncoderVersion = "7.4"
 val postgresqlVersion = "42.6.0"
-val flywayCoreVersion = "9.22.2"
-val springKafkaTestVersion = "3.0.11"
+val flywayCoreVersion = "9.22.3"
+val springKafkaTestVersion = "3.0.12"
 val springCloudContractVersion = "4.0.4"
 val testcontainersVersion = "1.19.1"
 val mockkVersion = "1.13.8"
 val assertJVersion = "3.24.2"
+val jsonAssertVersion = "1.5.1"
+val wiremockVersion = "2.35.1"
+val mockitoVersion = "5.1.0"
+val unleashVersion = "8.4.0"
 
 val snakeYamlVersion = "1.33" // siste er 2.x, men spring bruker 1.x
 val snappyJavaVersion = "1.1.10.5"
@@ -21,7 +25,7 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "1.9.10"
     id("org.jetbrains.kotlin.plugin.spring") version "1.9.10"
     id("org.jetbrains.kotlin.plugin.jpa") version "1.9.10"
-    id("org.springframework.boot") version "3.1.4"
+    id("org.springframework.boot") version "3.1.5"
     id("com.github.ben-manes.versions") version "0.49.0"
 }
 
@@ -54,7 +58,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework:spring-aspects")
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-    implementation("io.getunleash:unleash-client-java:8.3.1")
+    implementation("io.getunleash:unleash-client-java:$unleashVersion")
 
     // Internal libraries
     implementation("no.nav.pensjon.opptjening:omsorgsopptjening-domene-lib:$domeneVersion")
@@ -80,10 +84,10 @@ dependencies {
     testImplementation(kotlin("test"))
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.testcontainers:postgresql:$testcontainersVersion")
-    testImplementation("org.skyscreamer:jsonassert:1.5.1")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:5.1.0")
+    testImplementation("org.skyscreamer:jsonassert:$jsonAssertVersion")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:$mockitoVersion")
     testImplementation("io.mockk:mockk:${mockkVersion}")
-    testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:2.35.1")
+    testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:$wiremockVersion")
     testImplementation("org.assertj:assertj-core:$assertJVersion")
 }
 
