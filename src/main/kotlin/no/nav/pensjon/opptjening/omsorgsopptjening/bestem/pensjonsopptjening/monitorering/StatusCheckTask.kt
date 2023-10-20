4c6022574369f8e978f63d9724a70ecb7c9c4c04
Jan Kroken <jan.kroken@nav.com>
2023-10-20 12:01:41 +0200

    monitorering (pågående arbeid)

---
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/monitorering/StatusCheckTask.kt
@@ -0,0 +1,18 @@
+package no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.monitorering
+
+import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Scheduled
+
+
+class StatusCheckTask(private val statusRapporteringsService: StatusRapporteringCachingAdapter) {
+
+    companion object {
+        private val log = LoggerFactory.getLogger(this::class.java)
+    }
+
+    @Scheduled(fixedDelay = 120000, initialDelay = 20000)
+    fun check() {
+        log.info("Sjekker og oppdaterer status for overvåking")
+        val status = statusRapporteringsService.oppdaterRapporterbarStatus()
+    }
+}
