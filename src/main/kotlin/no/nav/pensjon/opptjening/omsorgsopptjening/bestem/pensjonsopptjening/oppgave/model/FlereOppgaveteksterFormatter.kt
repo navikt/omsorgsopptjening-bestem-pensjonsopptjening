c7bb9eda04666c1dff7a550e5850743b279f8b12
Jan Kroken <jan.kroken@nav.com>
2023-11-10 10:16:11 +0100

    liten forenkling av oppgavetekstformattering

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/oppgave/model/FlereOppgaveteksterFormatter.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/oppgave/model/FlereOppgaveteksterFormatter.kt
@@ -2,10 +2,6 @@ package no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.op
 
 internal data object FlereOppgaveteksterFormatter {
     fun format(oppgavetekster: Set<String>): String {
-        return if (oppgavetekster.isNotEmpty()) {
-            oppgavetekster.reduce { acc, s -> acc + "\n\n$s" }
-        } else {
-            ""
-        }
+        return oppgavetekster.joinToString(separator = "\n\n")
     }
 }
