646bba0339e520f44c5d9124de8c71be8e4ab982
Jan Kroken <jan.kroken@nav.no>
2024-07-31 22:26:44 +0200

    oppdatering av avhengigheter

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,24 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val domeneVersion = "1.0.69"
+val domeneVersion = "1.0.70"
 val azureAdClient = "0.0.7"
 val jacksonVersion = "2.17.1"
-val logbackEncoderVersion = "7.4"
+val logbackEncoderVersion = "8.0"
 val postgresqlVersion = "42.7.3"
 val flywayCoreVersion = "9.22.3" // 10.x krever nyere postgres-database
-val springKafkaTestVersion = "3.2.0"
+val springKafkaTestVersion = "3.2.2"
 val springCloudContractVersion = "4.0.4"
-val testcontainersVersion = "1.19.8"
-val mockkVersion = "1.13.11"
-val assertJVersion = "3.26.0"
-val jsonAssertVersion = "1.5.1"
-val wiremockVersion = "3.6.0"
-val mockitoVersion = "5.3.1"
-val unleashVersion = "9.2.2"
-val navTokenSupportVersion = "4.1.8"
+val testcontainersVersion = "1.20.1"
+val mockkVersion = "1.13.12"
+val assertJVersion = "3.26.3"
+val jsonAssertVersion = "1.5.3"
+val wiremockVersion = "3.9.1"
+val mockitoVersion = "5.4.0"
+val unleashVersion = "9.2.4"
+val navTokenSupportVersion = "5.0.1"
 val hibernateValidatorVersion = "8.0.1.Final"
+val junit5Version = "5.10.3"
 
 
 val snakeYamlVersion = "2.2"
@@ -30,7 +31,7 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.jpa") version kotlinVersion
-    id("org.springframework.boot") version "3.3.0"
+    id("org.springframework.boot") version "3.3.2"
     id("com.github.ben-manes.versions") version "0.51.0"
 }
 
@@ -97,8 +98,8 @@ dependencies {
     testImplementation("org.assertj:assertj-core:$assertJVersion")
     testImplementation("no.nav.security:token-validation-spring-test:$navTokenSupportVersion")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junit5Version")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junit5Version")
 }
 
 tasks.test {
