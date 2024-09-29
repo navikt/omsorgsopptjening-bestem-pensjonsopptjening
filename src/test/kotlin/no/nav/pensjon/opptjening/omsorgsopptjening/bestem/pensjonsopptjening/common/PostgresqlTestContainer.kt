00ee37633502599e83ac8a13b349e162e2840fd3
Jan Kroken <jan.kroken@nav.no>
2024-09-29 22:22:35 +0200

    PostgresSQL 16.4

--- src/test/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/common/PostgresqlTestContainer.kt
+++ src/test/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/common/PostgresqlTestContainer.kt
@@ -28,16 +28,16 @@ class PostgresqlTestContainer private constructor(image: String) : PostgreSQLCon
 
 
     companion object {
-        val instance: PostgresqlTestContainer = PostgresqlTestContainer("postgres:14.9-alpine")
+        val instance: PostgresqlTestContainer = PostgresqlTestContainer("postgres:16.4-alpine")
         private val dataSource = HikariDataSource(HikariConfig().apply {
-            jdbcUrl = "jdbc:tc:postgresql:14:///test"
+            jdbcUrl = "jdbc:tc:postgresql:16:///test"
             username = instance.username
             password = instance.password
         })
         fun createInstance(name: String): DataSource {
-            val instance = PostgresqlTestContainer("postgres:14.9-alpine")
+            val instance = PostgresqlTestContainer("postgres:16.4-alpine")
             val dataSource = HikariDataSource(HikariConfig().apply {
-                jdbcUrl = "jdbc:tc:postgresql:14:///$name"
+                jdbcUrl = "jdbc:tc:postgresql:16:///$name"
                 username = instance.username
                 password = instance.password
             })
