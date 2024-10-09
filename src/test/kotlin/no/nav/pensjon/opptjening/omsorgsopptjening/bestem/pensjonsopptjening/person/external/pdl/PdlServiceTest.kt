2591992c702cd9068528c5bfdc9028bc72a8d9e8
Jacob Meidell <jacob.meidell@nav.no>
2024-10-09 21:25:17 +0200

    Remove extra /

--- src/test/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/person/external/pdl/PdlServiceTest.kt
+++ src/test/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/person/external/pdl/PdlServiceTest.kt
@@ -39,7 +39,7 @@ internal class PdlServiceTest {
 
     private val pdlService: PdlService = PdlService(
         PdlClient(
-            pdlUrl = "${wiremock.baseUrl()}/$PDL_PATH",
+            pdlUrl = "${wiremock.baseUrl()}$PDL_PATH",
             tokenProvider = mock { on { getToken() }.thenReturn(MOCK_TOKEN) },
             metrics = mock(),
             graphqlQuery = GraphqlQuery(
