e66b494b5dd72cd7a44ef6b22a85d09c2bd325fd
Jacob Meidell <jacob.meidell@nav.no>
2024-12-17 10:32:06 +0100

    Add logging for toggle fetching

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/unleash/UnleashWrapper.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/bestem/pensjonsopptjening/unleash/UnleashWrapper.kt
@@ -1,6 +1,8 @@
 package no.nav.pensjon.opptjening.omsorgsopptjening.bestem.pensjonsopptjening.unleash
 
 import io.getunleash.Unleash
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.time.Clock
 import java.time.Duration
 import java.time.Instant
@@ -10,6 +12,7 @@ class UnleashWrapper(
     private val clock: Clock,
 ) {
     private val cache: MutableMap<NavUnleashConfig.Feature, CacheValue> = mutableMapOf()
+    private val log: Logger = LoggerFactory.getLogger(this::class.java)
 
     fun isEnabled(toggle: NavUnleashConfig.Feature): Boolean {
         return cache[toggle]?.let {
@@ -22,8 +25,12 @@ class UnleashWrapper(
     }
 
     private fun refreshAndGet(toggle: NavUnleashConfig.Feature): Boolean {
+        log.info("Refreshing toggle: ${toggle.toggleName}, existing value: ${cache[toggle]}")
         return unleash.isEnabled(toggle.toggleName)
-            .let { cache[toggle] = CacheValue(it, getNow()) }
+            .let {
+                cache[toggle] = CacheValue(it, getNow())
+                log.info("Toggle refreshed: ${toggle.toggleName}, new value: ${cache[toggle]}")
+            }
             .let { cache[toggle]!!.enabled }
     }
 
